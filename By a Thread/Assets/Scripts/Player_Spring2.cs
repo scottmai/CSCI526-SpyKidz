f9789373360e6db7d83025391e366ea56e003573
KISHAN THAKER <60134668+kthakerusc1@users.noreply.github.com>
2022-03-04 19:19:19 -0800

    Player Stick finction final

--- By a Thread/Assets/Scripts/Player_Spring2.cs
+++ By a Thread/Assets/Scripts/Player_Spring2.cs
@@ -5,16 +5,9 @@ using UnityEngine;
 public class Player_Spring2 : MonoBehaviour
 {
      public Rigidbody2D rb;
-
-    public float releaseTime = 0.15f;
-    private bool isPressed = false;
     private float velocity = 15f;
 
     void  Update() {
-        if(isPressed)
-        {
-            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
         
         if (Input.GetKey(KeyCode.DownArrow))
         {
@@ -26,24 +19,7 @@ public class Player_Spring2 : MonoBehaviour
             rb.bodyType = RigidbodyType2D.Dynamic;
         }
 
-        rb.MovePosition((new Vector3(Input.GetAxis("Horizontal"), 0, 0)*Time.deltaTime*velocity) + transform.position);
-    }
-    private void OnMouseDown() {
-        isPressed = true;
-        rb.isKinematic = true;
-    }
-
-    private void OnMouseUp() {
-        isPressed = false;
-        rb.isKinematic = false;
-
-        //StartCoroutine(Release());
+        //rb.MovePosition((new Vector3(Input.GetAxis("Horizontal"), 0, 0)*Time.deltaTime*velocity) + transform.position);
     }
     
-    IEnumerator Release()
-    {
-        yield return new WaitForSeconds(releaseTime);
-
-        GetComponent<SpringJoint2D>().enabled = false;
-    }
 }
