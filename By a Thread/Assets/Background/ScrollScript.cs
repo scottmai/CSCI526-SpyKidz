b841a6d7886cb98057deadd23469fb4b47d6f5bc
Vicky Yu <vickyyu@usc.edu>
2022-04-23 00:00:39 -0700

    updated art

---
+++ By a Thread/Assets/Background/ScrollScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    public float speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Time.time * speed, 0f);
+    }
+}
